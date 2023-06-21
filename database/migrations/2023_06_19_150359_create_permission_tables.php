7aa0d9aa14e417e1ff67d45c4e865dfe665f27f3
Raju Bepary <raju174829@gmail.com>
2023-06-21 17:18:33 +0600

    Edit permission and media migration
    - Add description column to permission table
    - Add function drop in media table

--- database/migrations/2023_06_19_150359_create_permission_tables.php
+++ database/migrations/2023_06_19_150359_create_permission_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\PermissionRegistrar;
 
 class CreatePermissionTables extends Migration
@@ -28,6 +28,7 @@ class CreatePermissionTables extends Migration
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MySQL 8.0 use string('name', 125);
+            $table->text('description');       // For MySQL 8.0 use string('name', 125);
             $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
 
